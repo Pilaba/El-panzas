fc95fb4295a5199afd2e8e24b808cc5d688597f5
Balde <pilaba@live.com>
2017-03-22 22:31:17 -0600

    Update InsertarUsuario.php

--- Carwash/InsertarUsuario.php
+++ Carwash/InsertarUsuario.php
@@ -4,7 +4,7 @@ $user =$_POST['usuario'];
 $email =$_POST['email'];
 $pass =$_POST['pass'];
 
-$result=mysql_query("INSERT INTO usuariossistem VALUES ('','$user', '$email','$pass',10)");
+$result=mysql_query("INSERT INTO usuariossistem VALUES ('','$user', '$email','$pass',1)");
 
 if($result==TRUE){
   echo ("Registro correcto");
